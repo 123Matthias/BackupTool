afb81d0a9a5d98ca0220af30704d3ff5f6880488
HPNOTEBOOK\leite <leitermatthias@gmx.at>
2025-01-10 22:58:45 +0100

    ausführbares jar versucht

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,6 @@
 module my.backup.backuptool {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
 
     opens my.backup.backupTool to javafx.fxml;
     opens my.backup.backupTool.Controller to javafx.fxml;
